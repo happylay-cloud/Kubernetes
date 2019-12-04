9bece4979d55b7c00e857096bad8160845f3e25b
happylay <1803455421@qq.com>
2019-12-04 15:09:42 +0800

    修改无法临时暴露环境变量错误

--- kubernetes v1.15.4/k8s.sh
+++ kubernetes v1.15.4/k8s.sh
@@ -50,11 +50,19 @@ fi;
 # 只在 master 节点执行
 # 替换 x.x.x.x 为 master 节点实际 IP（请使用内网 IP）
 # export 命令只在当前 shell 会话中有效，开启新的 shell 窗口后，如果要继续安装过程，请重新执行此处的 export 命令
-export MASTER_IP=$setmasterip
+
+echo "MASTER_IP=$setmasterip" >>  /etc/profile
+
 # 替换 apiserver.demo 为 您想要的 dnsName
-export APISERVER_NAME=$apiservername
+
+echo "APISERVER_NAME=$apiservername" >>  /etc/profile
+
 # Kubernetes 容器组所在的网段，该网段安装完成后，由 kubernetes 创建，事先并不存在于您的物理网络中
-export POD_SUBNET=$podsubnet
+
+echo "POD_SUBNET=$podsubnet" >>  /etc/profile
+
+source /etc/profile
+
 echo "${MASTER_IP}    ${APISERVER_NAME}" >> /etc/hosts
 
 if [ $ret -ne 0 ]; then
